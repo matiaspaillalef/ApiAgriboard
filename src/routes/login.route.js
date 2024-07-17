ea373c35e2c523a18f1e0d29da97e428b5df8624
Matias Paillalef Castro <m.paillalef.c@gmail.com>
2024-07-16 22:07:34 -0400

    login users and companies inactive

--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -1,4 +1,4 @@
-import {Router} from 'express'
+import { Router } from 'express'
 const router = Router()
 import bcrypt from "bcrypt";
 import mysql from 'mysql';
@@ -32,7 +32,7 @@ router.post('/login', (req, res) => {
         let { usuario, password } = req.body;
         var mysqlConn = mysql.createConnection(JSON.parse(process.env.DBSETTING));
 
-        mysqlConn.connect(function(err) {
+        mysqlConn.connect(function (err) {
 
             if (err) {
 
@@ -46,8 +46,10 @@ router.post('/login', (req, res) => {
 
             } else {
 
-                var queryString = "select  u.id,u.name,u.lastname,u.mail,u.id_rol,u.id_state, u.password ,u.id_company  from users u";
-                    queryString += " where mail='" + usuario + "'";
+                var queryString = "select  u.id,u.name,u.lastname,u.mail,u.id_rol,u.id_state as estado, u.password ,u.id_company, c.status from users u, companies c ";
+                queryString += " where mail='" + usuario + "'";
+                queryString += " and u.id_company = c.id";
+
                 console.log(queryString);
                 mysqlConn.query(queryString, function (error, results, fields) {
 
@@ -59,40 +61,54 @@ router.post('/login', (req, res) => {
                         }
 
                         res.json(jsonResult);
-        
+
                     } else {
-                        
-                        if (results  && results.length > 0) {
 
-                            //Javi coloque esto porque estaba agregando a la contraseña espacios enblanco y no los podia controlar
-                            const storedPasswordHash = results[0].password.trim();
-                            const passwordResult = bcrypt.compareSync(password, storedPasswordHash);
+                        if (results && results.length > 0) {
 
-                            if(passwordResult == true) {
-
-                                const jsonResult = {
-                                    "code": "OK",
-                                    "userId": results[0].id,
-                                    "nombre": results[0].name,
-                                    "apellido": results[0].lastname,
-                                    "mail": results[0].mail,
-                                    "rol": results[0].id_rol,
-                                    "estado": results[0].id_state,
-                                    "idCompany": results[0].id_company
-                                };
-
-                                res.json(jsonResult);
-
-                            } else {
+                            if (results[0].status == 2 || results[0].estado == 2) {
 
                                 const jsonResult = {
                                     "code": "ERROR",
-                                    "mensaje": "Usuario o contraseña incorrecta."
+                                    "mensaje": 'Empresa o usuario inactivo.'
                                 }
 
                                 res.json(jsonResult);
 
-                            } 
+                            } else {
+                                //Javi coloque esto porque estaba agregando a la contraseña espacios enblanco y no los podia controlar
+                                const storedPasswordHash = results[0].password.trim();
+                                const passwordResult = bcrypt.compareSync(password, storedPasswordHash);
+
+                                if (passwordResult == true) {
+
+                                    const jsonResult = {
+                                        "code": "OK",
+                                        "userId": results[0].id,
+                                        "nombre": results[0].name,
+                                        "apellido": results[0].lastname,
+                                        "mail": results[0].mail,
+                                        "rol": results[0].id_rol,
+                                        "estado": results[0].id_state,
+                                        "idCompany": results[0].id_company
+                                    };
+
+                                    res.json(jsonResult);
+
+                                } else {
+
+                                    const jsonResult = {
+                                        "code": "ERROR",
+                                        "mensaje": "Usuario o contraseña incorrecta."
+                                    }
+
+                                    res.json(jsonResult);
+
+                                }
+
+                            }
+
+
 
                         } else {
 
@@ -105,14 +121,14 @@ router.post('/login', (req, res) => {
                         }
                     }
                 });
-    
+
                 mysqlConn.end();
 
-            }           
+            }
         });
-    
+
     } catch (e) {
-		console.log(e);
+        console.log(e);
         res.json({ error: e })
     }
 });
