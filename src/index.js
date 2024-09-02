ffc6c3dcc3483236b25a89342f134c14b7995474
Matias Paillalef Castro <m.paillalef.c@gmail.com>
2024-09-02 12:34:43 -0400

    refactor: prod

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ import dashboardRouter from './routes/dashboard.route.js'
 
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT 
 
 app.use(cors());
 app.use(express.json());
@@ -46,4 +46,7 @@ console.log("*************************************************");
 console.log("*API AGRISOFT                                       *");
 console.log("*************************************************");
 
-app.listen(port, () => console.log('http://localhost:' + port));
+//app.listen(port, () => console.log('http://localhost:' + port));
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${port}`);
+});
