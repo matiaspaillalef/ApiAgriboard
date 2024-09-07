fac9b6eef5af6d3f1ece3041cb0a635ef2d12d61
Matias Paillalef Castro <m.paillalef.c@gmail.com>
2024-09-06 20:56:52 -0400

    index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,46 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
-import express from 'express'
-import cors from 'cors'
-import swaggerUi from 'swagger-ui-express'
-import compression from 'compression'
-import helmet from "helmet";
-import fs from 'fs'
+import express from 'express';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import compression from 'compression';
+import helmet from 'helmet';
+import fs from 'fs';
+import dotenv from 'dotenv';
+import https from 'https';
+
+// Cargar variables de entorno
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 4000;
 
 
-//const swaggerFile = JSON.parse(fs.readFileSync('./swagger-output.json', 'utf-8'));
-
-import tokenRouter from './routes/auth.route.js'
-import configurationRouter from './routes/configuration.route.js'
-import managementPeopleRouter from './routes/management-people.route.js'
-import loginRouter from './routes/login.route.js'
-import menuRouter from './routes/menu.route.js'
-import productionRouter from './routes/production.route.js'
-import dashboardRouter from './routes/dashboard.route.js'
-import alerts from './routes/alerts.route.js'
+const options = {
+    key: fs.readFileSync('./server.key'),
+    cert: fs.readFileSync('./server.cert')
+  };
 
 
-
-const app = express()
-const port = process.env.PORT
-
-app.use(cors());
+// Configurar middlewares
+app.use(cors({
+    origin: '*', // Permitir todas las solicitudes
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization, X-API-KEY',
+}));
 app.use(express.json());
 app.use(compression());
 app.use(helmet());
 
-app.use(express.static('public'));
+// Importar rutas
+import tokenRouter from './routes/auth.route.js';
+import configurationRouter from './routes/configuration.route.js';
+import managementPeopleRouter from './routes/management-people.route.js';
+import loginRouter from './routes/login.route.js';
+import menuRouter from './routes/menu.route.js';
+import productionRouter from './routes/production.route.js';
+import dashboardRouter from './routes/dashboard.route.js';
+import alerts from './routes/alerts.route.js'
 
+// Configurar rutas
 app.use('/api/v1', tokenRouter);
 app.use('/api/v1', loginRouter);
 app.use('/api/v1', menuRouter);
